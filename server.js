8ee1c752f15133f45d0aef05b32f921dff578182
mbergevoet <34296504+mbergevoet@users.noreply.github.com>
2021-03-09 15:41:45 +0100

    home page being able to render, WIP on detail page

--- server.js
+++ server.js
@@ -4,31 +4,75 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const fetch = require('node-fetch');
 
-// const baseUrl = "https://swapi.dev/api/";
-// const queryFilms = "films/";
-// const movieIdFix = [0, 4, 5, 6, 1, 2, 3];
+const swFilmsUrl = "https://swapi.dev/api/films";
+const detailUrl = "https://swapi.dev/api/films/";
+const movieIdFix = [0, 4, 5, 6, 1, 2, 3];
+
+
 
 // Middleware
-app.use(express.static('static'));
+app.use(express.static(`${__dirname}/static`));
 
 // Functions
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+app.set('views', path.join(`${__dirname}/views`));
 
-// Routes
+// Home Route
 app.get('/', (req, res) => {
-    res.send('Home Route');
+    fetch(swFilmsUrl)
+        .then((response) => {
+            const swApiResponse = response.json();
+            return swApiResponse;
+        })
+        .then((swData) => {
+            console.log(swData);
+            res.render('pages/index.ejs', { data: swData.results });
+        });
 });
 
-app.get('/films', (req, res) => {
-    res.render('pages/index.ejs');
-});
-
+// Detail Route
 app.get('/films/:id', (req, res) => {
-    res.render('pages/detail.ejs');
+    // console.log(detailUrl + movieIdFix[req.params.id]);
+    fetch(detailUrl + movieIdFix[req.params.id])
+        .then((response) => {
+            const swApiResponse = response.json();
+            return swApiResponse;
+        })
+        .then((swData) => {
+            const detailEndpoints = [swData.characters, swData.planets, swData.starships];
+            getDetailData(detailEndpoints);
+        })
+        .then((allEndpoints) => {
+            console.log(allEndpoints);
+            // res.render('pages/detail.ejs', { data: allEndpoints });
+        })
 });
 
-app.listen(3000, function () {
+// Helper functions
+function getDetailData(allEndpoints) {
+    const resultArray = allEndpoints.map(singleEndpoint => {
+        return singleEndpoint.forEach(endpoint => {
+            fetch(endpoint)
+                .then(response => {
+                    let responseData = response.json()
+                    responseData.then(result => {
+                        if (result.height) {
+                            displayCharacters(result);
+                        } else if (result.model) {
+                            displayStarships(result);
+                        } else {
+                            displayPlanets(result);
+                        }
+                    });
+                });
+        });
+    });
+    console.log(resultArray);
+    return resultArray;
+};
+
+app.listen(port, function () {
     console.log(`Server listening at http://localhost:${port}`);
 });
