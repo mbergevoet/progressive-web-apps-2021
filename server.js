16b258978aeebcb8d97634fdc7d932a614699d85
mbergevoet <34296504+mbergevoet@users.noreply.github.com>
2021-03-16 10:46:20 +0100

    added .env

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const http = require('http')
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT
 const path = require('path')
 const fetch = require('node-fetch')
 
