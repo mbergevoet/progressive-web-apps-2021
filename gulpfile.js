353debc61b5f8ac43e412d5b460e7da0a43a5d89
mbergevoet <34296504+mbergevoet@users.noreply.github.com>
2021-03-29 12:38:08 +0200

    gulp working

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,15 @@
 const gulp = require('gulp');
-const uglifycss = require('gulp-uglifycss');
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('message', function () {
-    return console.log('gulp is running')
+    return console.log('gulp is running');
 })
 
-gulp.task('css', function () {
-    gulp.src('./styles/style.css')
-        .pipe(uglifycss({
-            "maxLineLen": 80,
-            "uglyComments": true
-        }))
-        .pipe(gulp.dest('./static/min-styles/'));
+gulp.task('clean-css', function () {
+    return gulp
+        .src('./static/styles/*.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('static/minified'));
 });
 
-gulp.task('run'['css']);
-
-gulp.task('watch', function () {
-    gulp.watch('./styles/style.css', ['css']);
-})
-
-gulp.task('default', ['run', 'watch']);
+gulp.task('default', gulp.series('clean-css'));
